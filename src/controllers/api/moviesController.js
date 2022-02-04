7e87822634438aa87eb1277c78a55898360d188c
FranBulgarelli <bulgarellifrancisco@gmail.com>
2022-02-04 19:55:51 -0300

    subo proyecto creando nuestra API

---
+++ src/controllers/api/moviesController.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const db = require('../../database/models');
+const sequelize = db.sequelize;
+
+const Movies = db.Movie;
+const Genres = db.Genre;
+const Actors = db.Actor;
+
+module.exports = {
+    create: (req, res) => {
+        Movies.create({
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+        })
+        .then(movie => {
+            res.json(movie)
+        })
+        .catch(err => {
+            res.status(500).json({error : err})
+        })
+    },
+    destroy: (req, res) => {
+        
+        Movies.findByPk(req.params.id)
+        .then(movie => {
+            Movies.destroy({
+                where: {id: req.params.id}
+            })
+            .then(response => {
+                res.json(movie)
+            })
+            .catch(err => {
+                res.status(500).json({error : err})
+            })
+        })
+
+        .catch(err => {
+            res.status(500).json({error : err})
+        })
+    },
+}
